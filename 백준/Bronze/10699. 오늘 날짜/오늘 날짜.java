9c2e6c0f2ac5846faaff4950c4aae1e6a8a5ec50
s26047-code <s26047@gsm.hs.kr>
2026-04-08 19:40:58 +0900

    [Bronze V] Title: 오늘 날짜, Time: 96 ms, Memory: 14188 KB -BaekjoonHub

---
+++ 백준/Bronze/10699. 오늘 날짜/오늘 날짜.java
@@ -0,0 +1,11 @@
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        int year = 2026;
+        int month = 4;
+        int day = 8;
+        System.out.printf("%d-%02d-%02d", year, month, day);
+    }
+}
